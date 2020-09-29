989916a43eec32d4f731b47ff90dd783ba0cfc41
Mahasweta Chakraborti <mahaswec@usc.edu>
2020-09-29 17:03:32 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 cd /workspace/VL-BERT/data/conceptual-captions/utils/
 python gen_train4download.py
 sh download_train.sh
-cd ..
+cd /workspace/VL-BERT/data/conceptual-captions/train_image
 zip -0 ../train_image.zip ./*
 cd /workspace/VL-BERT/data/conceptual-captions/utils/
 python gen_train_image_json.py
